b99639eb6b5db5dc3b55440f77e280b4b5e6382c
Aliseiko <aliseiko.dev@gmail.com>
2022-11-12 00:05:47 +0300

    feat: add redirect to / if user is logged

--- src/pages/Auth.tsx
+++ src/pages/Auth.tsx
@@ -3,11 +3,17 @@ import Button from '../components/Button/Button';
 import styles from './Auth.module.scss';
 import LoginForm from '../components/AuthForms/LoginForm';
 import RegisterForm from '../components/AuthForms/RegisterForm';
+import { authService } from '../api/authService';
+import { Navigate } from 'react-router-dom';
 
 const Auth: React.FC = () => {
   const [isLoginFormActive, setIsLoginFormActive] = useState(true);
   const loginButtonStyle = !isLoginFormActive ? styles.deactivated : '';
   const registerButtonStyle = isLoginFormActive ? styles.deactivated : '';
+
+  if (authService.isUserLogged()) {
+    return <Navigate replace to="/" />;
+  }
 
   return (
     <div className={styles.page}>

bb9b4273f9c29c0c7b5509508747ace9b7081e15
Aliseiko <aliseiko.dev@gmail.com>
2022-11-17 22:24:18 +0300

    refactor: temp buttons on welcome page

--- src/pages/Welcome/index.tsx
+++ src/pages/Welcome/index.tsx
@@ -1,14 +1,36 @@
 import React from 'react';
 
-import Footer from 'components/Footer';
-
 import styles from './Welcome.module.scss';
+import Button from '../../components/Button/Button';
+import { useNavigate } from 'react-router-dom';
 
 const Welcome: React.FC = () => {
+  const navigate = useNavigate();
+
   return (
     <div className={styles.welcome}>
       <div>RSS final task: Project management app</div>
-      <Footer />
+      <Button
+        onClick={() => {
+          navigate('/auth');
+        }}
+      >
+        Auth
+      </Button>
+      <Button
+        onClick={() => {
+          navigate('/profile');
+        }}
+      >
+        Profile
+      </Button>
+      <Button
+        onClick={() => {
+          navigate('/boards');
+        }}
+      >
+        Boards
+      </Button>
     </div>
   );
 };

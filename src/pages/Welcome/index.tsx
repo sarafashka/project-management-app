6364593162ae9f0e0a91ebc95babe5ffaf696c72
zhadan93 <zhadan.evgenya@yandex.ru>
2022-11-17 03:32:22 +0300

    fix: change welcome page style

--- src/pages/Welcome/index.tsx
+++ src/pages/Welcome/index.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 
 import Footer from 'components/Footer';
 
+import styles from './Welcome.module.scss';
+
 const Welcome: React.FC = () => {
   return (
-    <>
-      <div className="App">RSS final task: Project management app</div>
+    <div className={styles.welcome}>
+      <div>RSS final task: Project management app</div>
       <Footer />
-    </>
+    </div>
   );
 };
 

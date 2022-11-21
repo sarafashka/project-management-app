3b26034a5371a1dec4ba62519bc6e0909f10c120
Aliseiko <aliseiko.dev@gmail.com>
2022-11-21 22:19:53 +0300

    feat: add responsive styles

--- src/pages/Welcome/index.tsx
+++ src/pages/Welcome/index.tsx
@@ -6,6 +6,7 @@ import { useNavigate } from 'react-router-dom';
 import tryImg from '../../assets/img/welcome-try.png';
 import TeamMemberCard from '../../components/TeamMemberCard/TeamMemberCard';
 import teamMembers from '../../constants/team';
+import dndimg from '../../assets/img/DnDtemp.png';
 
 const Welcome: React.FC = () => {
   const navigate = useNavigate();
@@ -30,55 +31,58 @@ const Welcome: React.FC = () => {
             quickest way.
           </p>
         </div>
-        <img className={styles.dragImg} src="/" alt="Drag and Drop image" />
+        <img className={styles.dragImg} src={dndimg} alt="Drag and Drop image" />
       </section>
-      <section className={styles.try}>
-        <img className={styles.tryImg} src={tryImg} alt="Try App image" />
-        <div className={styles.tryTextBlock}>
-          <h2 className={styles.tryTitle}>Try Project Management App Now!</h2>
-          <ul className={styles.tryList}>
-            <li className={styles.tryListItem}>
-              Collaborate, manage projects, and reach new productivity peaks
-            </li>
-            <li className={styles.tryListItem}>Manage your boards using Drag-n-Drop</li>
-            <li className={styles.tryListItem}>
-              You can specify additional info in task description
-            </li>
-            <li className={styles.tryListItem}>
-              Convenient search among the list of all your projects
-            </li>
-            <li className={styles.tryListItem}>
-              Unlimited boards, columns and tasks for all registered users
-            </li>
-          </ul>
-          <div className={styles.tryButtons}>
-            <Button
-              className={styles.tryButton}
-              onClick={() => {
-                navigate('/auth');
-              }}
-            >
-              Sign In / Register
-            </Button>
-            <Button
-              className={styles.tryButton}
-              onClick={() => {
-                navigate('/profile');
-              }}
-            >
-              Profile
-            </Button>
-            <Button
-              className={styles.tryButton}
-              onClick={() => {
-                navigate('/boards');
-              }}
-            >
-              Boards
-            </Button>
+      <section className={styles.tryApp}>
+        <div className={styles.try}>
+          <img className={styles.tryImg} src={tryImg} alt="Try App image" />
+          <div className={styles.tryTextBlock}>
+            <h2 className={styles.tryTitle}>Try Project Management App Now!</h2>
+            <ul className={styles.tryList}>
+              <li className={styles.tryListItem}>
+                Collaborate, manage projects, and reach new productivity peaks
+              </li>
+              <li className={styles.tryListItem}>Manage your boards using Drag-n-Drop</li>
+              <li className={styles.tryListItem}>
+                You can specify additional info in task description
+              </li>
+              <li className={styles.tryListItem}>
+                Convenient search among the list of all your projects
+              </li>
+              <li className={styles.tryListItem}>
+                Unlimited boards, columns and tasks for all registered users
+              </li>
+            </ul>
+            <div className={styles.tryButtons}>
+              <Button
+                className={styles.tryButton}
+                onClick={() => {
+                  navigate('/auth');
+                }}
+              >
+                Sign In / Register
+              </Button>
+              <Button
+                className={styles.tryButton}
+                onClick={() => {
+                  navigate('/profile');
+                }}
+              >
+                Profile
+              </Button>
+              <Button
+                className={styles.tryButton}
+                onClick={() => {
+                  navigate('/boards');
+                }}
+              >
+                Boards
+              </Button>
+            </div>
           </div>
         </div>
       </section>
+
       <section className={styles.team}>
         <h2 className={styles.teamTitle}>Our team</h2>
         <div className={styles.teamMembers}>

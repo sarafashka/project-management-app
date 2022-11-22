f27f8158baa60f35b3eb1154d246aa6f7a89a0ab
sarafashka <96177099+sarafashka@users.noreply.github.com>
2022-11-22 15:11:57 +0200

    refactor: separate deleting column

--- src/components/Column/Column.tsx
+++ src/components/Column/Column.tsx
@@ -1,25 +1,22 @@
-import React, { useState } from 'react';
-import cn from 'classnames';
+import React from 'react';
 import styles from './Column.module.scss';
 import Button from 'components/Button/Button';
-import { deleteColumn, updateColumn } from 'store/taskSlice/columnThunk';
+import { updateColumn } from 'store/taskSlice/columnThunk';
 import { useAppDispatch, useAppSelector } from 'hooks/reduxTypedHooks';
-import { CloseModalEvent, GetBoardByIdColumnData, RequestDeleteColumn } from 'types/types';
-import ColumnTitle from './ColumnTitle';
+import { GetBoardByIdColumnData } from 'types/types';
+import ColumnTitle from './ColumnTitle/ColumnTitle';
 import Task from 'components/Task';
 import { createTask } from 'store/taskSlice/taskThunk';
 import { selectUser } from 'store/selectors/selectors';
 import { selectTasksList } from 'store/selectors/selectors';
 import { findColumn } from 'utils/utils';
-import Modal from 'components/Modal';
-import ConfirmationModal from 'components/Modal/ConfirmationModal';
+import ColumnDelete from './ColumnDelete';
 
 type Props = {
   id: string;
 };
 
 const Column: React.FC<Props> = (column) => {
-  const [isOpen, setIsOpen] = useState(false);
   const { id } = column;
   const dispatch = useAppDispatch();
   const user = useAppSelector(selectUser);
@@ -52,46 +49,19 @@ const Column: React.FC<Props> = (column) => {
     },
   }; // delete when modal will be finish
 
-  const handleClick = () => {
-    const dataForDeleteColumn: RequestDeleteColumn = {
-      columnId: id,
-      boardId: board.id,
-    };
-    dispatch(deleteColumn(dataForDeleteColumn));
-  };
-
-  const openModal = (event: CloseModalEvent) => {
-    event.preventDefault();
-    setIsOpen(true);
-  };
-
-  const closeModal = () => {
-    setIsOpen(false);
-  };
-
   return (
     <>
       <div className={styles.item}>
         <div className={styles.header}>
           <p className={styles.count}>({tasks.length})</p>
           <ColumnTitle title={title} submit={handleSubmit} />
-          <Button className={cn(styles.button)} type="button" kind="delete" onClick={openModal} />
+          <ColumnDelete columnId={id} boardId={board.id} title={title} />
         </div>
-
         {<Task columnId={id} />}
-
         <Button className={styles.newTask} onClick={() => dispatch(createTask(dataForCreateTask))}>
           + Add a task
         </Button>
       </div>
-      <Modal kind="confirmation" onClose={closeModal} isOpen={isOpen}>
-        <ConfirmationModal
-          entity="column"
-          value={title}
-          onConfirm={handleClick}
-          onCancel={closeModal}
-        />
-      </Modal>
     </>
   );
 };

513726cac69230c843ea05dc6d6d338cfe3b8f30
zhadan93 <zhadan.evgenya@yandex.ru>
2022-11-29 18:32:01 +0300

    fix: reset document scrolling when opening a modal window

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useState } from 'react';
 import classNames from 'classnames';
 import { useLocation } from 'react-router-dom';
 
@@ -27,20 +27,14 @@ const { header, container, btnContainer, sticky } = styles;
 
 type HeaderProps = {
   className?: string;
+  isSticky: boolean;
 };
 
-const Header: React.FC<HeaderProps> = ({ className }) => {
+const Header: React.FC<HeaderProps> = ({ className, isSticky }) => {
   const { pathname } = useLocation();
-  const headerRef = useRef<HTMLElement>(null);
   const dispatch = useAppDispatch();
   const [isOpen, setIsOpen] = useState(false);
   const { t } = useTranslation('translation');
-
-  const changeHeaderStyle = () => {
-    window.scrollY > 0
-      ? headerRef.current?.classList.add(sticky)
-      : headerRef.current?.classList.remove(sticky);
-  };
 
   const handleCreateClick = (formData: DataFromEditForm) => {
     dispatch(
@@ -56,15 +50,9 @@ const Header: React.FC<HeaderProps> = ({ className }) => {
     setIsOpen((prevState) => !prevState);
   };
 
-  useEffect(() => {
-    window.addEventListener('scroll', changeHeaderStyle);
-
-    return () => window.removeEventListener('scroll', changeHeaderStyle);
-  }, []);
-
   return (
     <>
-      <header className={classNames(header, className)} ref={headerRef}>
+      <header className={classNames(header, { [`${sticky}`]: isSticky }, className)}>
         <div className={container}>
           <Logo />
           <div className={btnContainer}>
--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { Outlet } from 'react-router-dom';
 
 import Header from 'components/Header';
@@ -7,18 +7,35 @@ import ErrorBoundary from '../ErrorBoundary/ErrorBoundary';
 
 import styles from './Layout.module.scss';
 
-const { layout } = styles;
+const { layout, wrapper } = styles;
 
 const Layout = (): JSX.Element => {
+  const layoutRef = useRef<HTMLDivElement>(null);
+  const [isSticky, setIsSticky] = useState(false);
+
+  const changeHeaderStyle = () => {
+    const scrollTop = layoutRef.current?.scrollTop;
+    scrollTop !== undefined && setIsSticky(scrollTop > 0);
+  };
+
+  useEffect(() => {
+    const layoutEl = layoutRef.current;
+    layoutEl?.addEventListener('scroll', changeHeaderStyle);
+
+    return () => layoutEl?.removeEventListener('scroll', changeHeaderStyle);
+  }, []);
+
   return (
-    <div className={layout}>
-      <Header />
-      <main>
-        <ErrorBoundary>
-          <Outlet />
-        </ErrorBoundary>
-      </main>
-      <Footer />
+    <div className={wrapper} ref={layoutRef}>
+      <div className={layout}>
+        <Header isSticky={isSticky} />
+        <main>
+          <ErrorBoundary>
+            <Outlet />
+          </ErrorBoundary>
+        </main>
+        <Footer />
+      </div>
     </div>
   );
 };

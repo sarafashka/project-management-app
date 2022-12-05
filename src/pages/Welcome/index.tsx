b713c85600974002ebd31f752f2304bf552de838
Aliseiko <aliseiko.dev@gmail.com>
2022-12-05 22:10:37 +0300

    fix: add names translations

--- src/pages/Welcome/index.tsx
+++ src/pages/Welcome/index.tsx
@@ -24,22 +24,22 @@ const Welcome: React.FC = () => {
       cardText: t('ivan'),
       photoLink: 'https://avatars.githubusercontent.com/u/86059997?v=4',
       githubLink: 'https://github.com/Aliseiko',
-      name: 'Ivan Aliseiko',
-      role: 'Software Developer',
+      name: t('ivan-name'),
+      role: 'Frontend Developer',
     },
     {
       cardText: t('nataly'),
       photoLink: 'https://avatars.githubusercontent.com/u/96177099?v=4',
       githubLink: 'https://github.com/sarafashka',
-      name: 'Nataly Stepanova',
-      role: 'Software Developer',
+      name: t('nataly-name'),
+      role: 'Frontend Developer',
     },
     {
       cardText: t('evgeniia'),
       photoLink: 'https://avatars.githubusercontent.com/u/78043992?v=4',
       githubLink: 'https://github.com/zhadan93',
-      name: 'Evgeniia Zhadan',
-      role: 'Software Developer',
+      name: t('evgeniia-name'),
+      role: 'Frontend Developer',
     },
   ];
   const dispatch = useAppDispatch();
